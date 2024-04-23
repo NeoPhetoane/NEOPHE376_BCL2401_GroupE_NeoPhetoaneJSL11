ae7a9417211b2d8f32db0bab9653923c08bf9602
NeoPhetoane <phetoaneneo4@gmail.com>
2024-04-23 13:48:15 +0200

    Compareson operator added to filterAndDisplayTasksByBoard

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function displayBoards(boards) {
 // TASK: Fix Bugs
 function filterAndDisplayTasksByBoard(boardName) {
   const tasks = getTasks(); // Fetch tasks from a simulated local storage function
-  const filteredTasks = tasks.filter(task => task.board = boardName);
+  const filteredTasks = tasks.filter(task => task.board === boardName); //Assignment property changed to comparison
 
   // Ensure the column titles are set outside of this function or correctly initialized before this function runs
 
